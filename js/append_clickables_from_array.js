bd6efc4617ca6888a944c71503faed22ae10aaf4
KoreaHaos <koreahaos@gmail.com>
2017-01-15 16:54:20 +0000

    Clean up style.

--- js/append_clickables_from_array.js
+++ js/append_clickables_from_array.js
@@ -8,8 +8,8 @@ function create_clickable_element_for_url(script_num_in, label_for_clickable_url
     insert_string += '  <div id="clickable_command_div_' + script_num_in + '">';
     insert_string += '    <label for="clickable_command_label_' + script_num_in + '">' + label_for_clickable_url + '</label>';
     insert_string += '    <input type="text" id="clickable_command_' + script_num_in + '" value="bash <(curl -s https://gist.githubusercontent.com/' + url_for_clickable + '/raw)" />';
-    insert_string += '    <button data_tooltip="Copy Command to Clipboard." data-copytarget="#clickable_command_' + script_num_in + '">Click Copy</button>';
-    insert_string += '    <button data_tooltip="View Script at GitHub." onclick=" window.open(\'https://gist.github.com/' + url_for_clickable + '\',\'_blank\')">View Gist</button>';
+    insert_string += '    <button class="leftest_button" data_tooltip="Copy Command to Clipboard." data-copytarget="#clickable_command_' + script_num_in + '">Click Copy</button>';
+    insert_string += '    <button  class="rightest_button" data_tooltip="View Script at GitHub." onclick=" window.open(\'https://gist.github.com/' + url_for_clickable + '\',\'_blank\')">View Gist</button>';
     insert_string += '  </div>';
 
     temp.innerHTML = insert_string;
