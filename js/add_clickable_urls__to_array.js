b34699d4a2b6640d0aeac3094b248b8b33b0b6e5
KoreaHaos <koreahaos@gmail.com>
2017-01-15 15:06:38 +0000

    Add favicon to page.

--- js/add_clickable_urls__to_array.js
+++ js/add_clickable_urls__to_array.js
@@ -1,6 +1,6 @@
 var clickable_script_urls = [
     ["Setup Haos Bash on Cloud9!", "EntropyHaos/4617f56f010f0b476f0aa969574794a3"],
-    ["Setup Jekyll on Cloud9.", "EntropyHaos/86d8fd337e4de35884ae059b52bfe8bc"],
+    ["Setup Jekyll on Cloud9 and Start Develoment Server.", "EntropyHaos/86d8fd337e4de35884ae059b52bfe8bc"],
     ["Setup Java Spark Framework on Cloud9.", "EntropyHaos/69ecc48cd9dd4d7d224a9addc0e16a84"],
     ["Create Reveal.JS GH-Pages site using Cloud9.", "EntropyHaos/f5571a1430d5eec65055afbfb7358c62"]
 ];
