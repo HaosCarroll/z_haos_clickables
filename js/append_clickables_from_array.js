15b5566d375c1695e9c1786b370d5e70291e2def
Copernican System -- Monmatia --         지구 행성 <EntropyHaos@users.noreply.github.com>
2017-01-15 08:15:05 -0600

    Update append_clickables_from_array.js

--- js/append_clickables_from_array.js
+++ js/append_clickables_from_array.js
@@ -1,9 +1,3 @@
-var clickable_script_urls = [
-    ["Setup Haos Bash on Cloud9!", "EntropyHaos/4617f56f010f0b476f0aa969574794a3"],
-    ["Setup Java Spark Framework on Cloud9.", "EntropyHaos/69ecc48cd9dd4d7d224a9addc0e16a84"],
-    ["Create Reveal.JS GH-Pages site using Cloud9.", "EntropyHaos/f5571a1430d5eec65055afbfb7358c62"]
-];
-
 function create_clickable_element_for_url(script_num_in, label_for_clickable_url, url_for_clickable) {
 
     var frag = document.createDocumentFragment();
